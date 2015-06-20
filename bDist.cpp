2e0e678dc17e6e883f8e6b350083d7f2bd5d3aa1
Nick Kennedy <nick@nick-kennedy.com>
2015-06-20 23:04:38 +0100

    Updated to use 64 bit integer calculation rather than just doing it byte by byte (on a 64 bit processor should lead to an 8 fold increase in speed)

--- bDist.cpp
+++ bDist.cpp
@@ -1,27 +1,47 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-unsigned int countBits(unsigned int x) {
-  unsigned int c;
-  x = x - ((x >> 1) & 0x55555555);                    // reuse input as temporary
-  x = (x & 0x33333333) + ((x >> 2) & 0x33333333);     // temp
-  c = ((x + (x >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
-  return (c);
+//countBits function taken from https://en.wikipedia.org/wiki/Hamming_weight#Efficient_implementation
+
+const uint64_t m1  = 0x5555555555555555; //binary: 0101...
+const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
+const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+
+int countBits(uint64_t x) {
+  x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
+  x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits 
+  x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits 
+  return (x * h01)>>56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ... 
+}
+
+// [[Rcpp::export]]
+int countBitsFromRaw(RawVector rv) {
+  uint64_t* x = (uint64_t*)RAW(rv);
+  return(countBits(*x));
 }
 
 // [[Rcpp::export]]
 NumericVector bDist(RawMatrix mat) {
   int nr(mat.nrow()), nc(mat.ncol());
-  NumericVector res(nr * (nr - 1) / 2);
-  long a(0);
-  for (int i = 0; i < nr - 1; i++) {
-    for (int j = i + 1; j < nr; j++) {
-      unsigned int sx(0), so(0);
-      for (int k = 0; k < nc; k++) {
-        unsigned int o = mat(i, k) | mat(j, k);
+  int nw = nr / 8;
+  NumericVector res(nc * (nc - 1) / 2);
+  // Access the raw data as unsigned 64 bit integers
+  uint64_t* data = (uint64_t*)RAW(mat);
+  uint64_t a(0);
+  // Work through each possible combination of columns (rows in the original integer matrix)
+  for (int i = 0; i < nc - 1; i++) {
+    for (int j = i + 1; j < nc; j++) {
+      uint64_t sx = 0;
+      uint64_t so = 0;
+      // Work through each 64 bit integer and calculate the sum of (x ^ y) and (x | y)
+      for (int k = 0; k < nw; k++) {
+        uint64_t o = data[nw * i + k] | data[nw * j + k];
+        // If (x | y == 0) then (x ^ y) will also be 0
         if (o) {
+          // Use Hamming weight method to calculate number of set bits
           so = so + countBits(o);
-          unsigned int x = mat(i, k) ^ mat(j, k);
+          uint64_t x = data[nw * i + k] ^ data[nw * j + k];
           if (x) {
             sx = sx + countBits(x);
           }
--- binaryDist.R
+++ binaryDist.R
@@ -1,50 +1,100 @@
-myDist <- function(x) {
-  x <- x[, colSums(x) > 0]
-  dist(x, "binary")
-}
-
 library("Rcpp")
 library("plyr")
 sourceCpp("bDist.cpp")
 
 # Converts a binary integer vector into a packed raw vector,
-# padding out at the end to make the input length a multiple of 8
-packRow <- function(row) {
-  packBits(as.raw(c(row, rep(0, (8 - length(row)) %% 8 ))))
+# padding out at the end to make the input length a multiple of packWidth
+packRow <- function(row, packWidth = 64L) {
+  packBits(as.raw(c(row, rep(0, (packWidth - length(row)) %% packWidth))))
 }
 
-makeRandomData <- function(nr, nc, maxBits, packed = FALSE) {
-  t(replicate(nr, {
-    y <- integer(nc)
-    y[sample(nc, sample(maxBits, 1))] <- 1L
+as.PackedMatrix <- function(x, packWidth = 64L) {
+  UseMethod("as.PackedMatrix")
+}
+
+# Converts a binary integer matrix into a packed raw matrix
+# padding out at the end to make the input length a multiple of packWidth
+as.PackedMatrix.matrix <- function(x, packWidth = 64L) {
+  stopifnot(packWidth %% 8 == 0, class(x) %in% c("matrix", "Matrix"))
+  storage.mode(x) <- "raw"
+  if (ncol(x) %% packWidth != 0) {
+    x <- cbind(x, matrix(0L, nrow = nrow(x), ncol = packWidth - (ncol(x) %% packWidth)))
+  }
+  out <- packBits(t(x))
+  dim(out) <- c(ncol(x) %/% 8, nrow(x))
+  class(out) <- "PackedMatrix"
+  out
+}
+
+# Converts back to an integer matrix
+as.matrix.PackedMatrix <- function(x) {
+  out <- rawToBits(x)
+  dim(out) <- c(nrow(x) * 8L, ncol(x))
+  storage.mode(out) <- "integer"
+  t(out)
+}
+
+# Generates random sparse data for testing the main function
+makeRandomData <- function(nObs, nVariables, maxBits, packed = FALSE) {
+  x <- replicate(nObs, {
+    y <- integer(nVariables)
+    y[sample(nVariables, sample(maxBits, 1))] <- 1L
     if (packed) {
-      packBits(y)
+      packRow(y, 64L)
     } else {
       y
     }
-  }))
-}
-
-y <- makeRandomData(2000, 400, maxBits = 5, packed = TRUE)
-
-padPackMatrix <- function(x) {
-  storage.mode(x) <- "raw"
-  if (ncol(x) %% 8 != 0) {
-    x <- cbind(x, matrix(0, nrow = nrow(x), ncol = 8 - (ncol(x) %% 8)))
+  })
+  if (packed) {
+    class(x) <- "PackedMatrix"
+    x
+  } else {
+    t(x)
   }
-  aaply(x, 1, packBits)
 }
 
+# Reads a binary matrix from file or character vector
+# Borrows the first bit of code from read.table
+readPackedMatrix <- function(file = NULL, text = NULL, packWidth = 64L) {
+  if (missing(file) && !missing(text)) {
+    file <- textConnection(text)
+    on.exit(close(file))
+  }
+  if (is.character(file)) {
+    file <- file(file, "rt")
+    on.exit(close(file))
+  }
+  if (!inherits(file, "connection")) 
+    stop("'file' must be a character string or connection")
+  if (!isOpen(file, "rt")) {
+    open(file, "rt")
+    on.exit(close(file))
+  }
+  lst <- list()
+  i <- 1
+  while(length(line <- readLines(file, n = 1)) > 0) {
+    lst[[i]] <- packRow(as.integer(strsplit(line, "", fixed = TRUE)[[1]]), packWidth = packWidth)
+    i <- i + 1
+  }
+  out <- do.call("cbind", lst)
+  class(out) <- "PackedMatrix"
+  out
+}
+
+# Wrapper for the C++ code which 
 binaryDist <- function(x) {
-  if (storage.mode(x) != "raw") {
-    x <- padPackMatrix(x)
+  if (class(x) != "PackedMatrix") {
+    x <- as.PackedMatrix(x)
   }
   dst <- bDist(x)
-  class(dst) <- "dist"
-  attr(dst, "Size") <- nrow(x)
-  attr(dst, "Diag") <- attr(dst, "Upper") <- "FALSE"
+  attr(dst, "Size") <- ncol(x)
+  attr(dst, "Diag") <- attr(dst, "Upper") <- FALSE
   attr(dst, "method") <- "binary"
+  attr(dst, "call") <- match.call()
+  class(dst) <- "dist"
   dst
 }
 
-system.time(bd <- binaryDist(y))
+x <- makeRandomData(2000, 400, maxBits = 5, packed = TRUE)
+
+system.time(bd <- binaryDist(x))
